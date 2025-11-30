5281d0aaf041475c98e01037e61c3db22d7ca0e7
RK <AWSDevOpsProfessional2023@gmail.com>
2025-11-30 11:28:56 -0800

    Modify#: 6

--- terraform/environments/dev/main.tf
+++ terraform/environments/dev/main.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket  = "eks-devops-tf-backend"
+    bucket  = "eks-devops-tf-backendg"
     key     = "eks/dev/terraform.tfstate"
-    region  = "ap-south-1"
+    region  = "us-west-2"
     encrypt = true
   }
 }
--- terraform/modules/eks/main.tf
+++ terraform/modules/eks/main.tf
@@ -22,7 +22,7 @@ module "eks" {
 
   access_entries = {
     github_actions = {
-      principal_arn     = "arn:aws:iam::666930281169:user/GitHub-Actions"
+      principal_arn     = "arn:aws:iam::400338099943:user/GitHub-Actions"
       kubernetes_groups = ["cluster-admin"]
     }
   }

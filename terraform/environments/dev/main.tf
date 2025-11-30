01795b978b8e8eb87b3c13196491b6ae5276de5e
RK <AWSDevOpsProfessional2023@gmail.com>
2025-11-29 19:39:35 -0800

    1st Modify

--- terraform/environments/dev/main.tf
+++ terraform/environments/dev/main.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket  = "eks-devops-tf-backend"
     key     = "eks/dev/terraform.tfstate"
-    region  = "ap-south-1"
+    region  = "us-west-2"
     encrypt = true
   }
 }

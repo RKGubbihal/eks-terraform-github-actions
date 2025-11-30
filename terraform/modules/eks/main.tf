793af12990f3f0bf72640e62fd184573080cf83b
RK <AWSDevOpsProfessional2023@gmail.com>
2025-11-30 11:38:20 -0800

    Modify#: 7

--- terraform/modules/eks/main.tf
+++ terraform/modules/eks/main.tf
@@ -22,7 +22,7 @@ module "eks" {
 
   access_entries = {
     github_actions = {
-      principal_arn     = "arn:aws:iam::400338099943:user/AIDevOpsUser"
+      principal_arn     = "arn:aws:iam::400338099943:user/GitHub-Actions"
       kubernetes_groups = ["cluster-admin"]
     }
   }
